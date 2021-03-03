a05702f212630350dc93a36d9c912b30edee9327
nandomgu <nando.mgu@gmail.com>
2021-03-03 23:42:03 +0100

    Update covid_analysis_main.R

--- covid_analysis_main.R
+++ covid_analysis_main.R
@@ -1,6 +1,6 @@
 #Single-cell analysis for Agerer et al, 2021
 #Author: Luis Fernando Montaño-Gutierrez
-#Institution: Developmental Cancer genomics Group, St. Anna Children's Cancer Research Insitute, Vienna, Austria.
+#Affiliation: Developmental Cancer genomics Group, St. Anna Children's Cancer Research Insitute, Vienna, Austria.
 
 #Importing libraries
 library(Seurat)
